2f0bc02f2c289cc4dc5f8deafa7fe6f135ec9775
Aron Choe <82278879+A-Choe-Boi@users.noreply.github.com>
2023-02-03 12:42:40 -0500

    Update vibration_motor.ino

--- vibration_motor/vibration_motor.ino
+++ vibration_motor/vibration_motor.ino
@@ -52,6 +52,78 @@ void vibrate_old(int n_pulse, int pulse_strength) {
   }
 }
 
+/* TEST FUNCTION
+    -gets a random number
+    -determines the intensity of the vibrate function
+*/
+int randomVal = random(0, 300);
+void getValue(){
+  // map --> change the proportion of randomVal/300 to #/255
+  int prop = map(randomVal, 0, 300, 80, 255);
+  // divides into 4 equal sections
+  int divvy = (255-80)/4;
+  // Loops through "i" to determine the vibration strength
+  for(int i = 1; i < 4; i++){
+    // i = 1 --> 25%; i = 2 --> 50%; i = 3 --> 75%
+    if(prop <= (divvy*i)+80){
+      vibrate_old(i, prop);
+    }
+  }
+  vibrate_old(4, prop);
+}
+
+/* TEST FUNCTION REPLICA
+    -gets a random number
+    -determines the intensity of the vibrate function
+    -SPECIALITY: if simpler as it uses a series of if-statements rather than a for-loop
+*/
+void getValue2(){
+  // map --> change the proportion of randomVal/300 to #/255
+  int prop = map(randomVal, 0, 300, 80, 255);
+  // divides into 4 equal sections
+  int divvy = (255-80)/4;
+  // checks which "proportion" the map value equals to and determines the strength of the vibration
+  if(prop < divvy * 1){
+    vibrate(1, prop);
+  } else if(prop < divvy * 2){
+    vibrate(2, prop);
+  } else if(prop < divvy * 3){
+    vibrate(3, prop);
+  } else {
+    vibrate(4, prop);
+  }
+}
+
+/* TEST FUNCTION ON STEROIDS
+    -gets a random number
+    -determines the intensity of the vibrate function
+    -determines the amount of motors that should be used
+*/
+void getValue3(){
+  // map --> change the proportion of randomVal/300 to #/255
+  int prop = map(randomVal, 0, 300, 80, 255);
+  // divides into 3 equal sections --> for number of sensors
+  int divvy = (255-80)/3;
+  // divides each section into 4 parts --> for strength of the sensor
+  int divvyUp = divvy/4;
+  // Loops through "i" to determine the vibration strength
+  for(int i = 1; i < 4; i++){
+    // i = 1 --> 25%; i = 2 --> 50%; i = 3 --> 75%
+    if(prop <= (divvy*i)+80){
+      int numberOfMotors = i;
+      int determinant = prop - (divvy i1- * 80 +); // postfix of: divvy * (i - 1) + 80
+      for(int j = 1; j < 5; j++){
+        if(determinant < divvyUp * j){
+          vibrate_old(j, prop);
+        }
+      }
+      /* 1) basically should determine that everything equal to and less than the number of motors should be used
+            i) if the motor is "lower", then make all levels buzz.
+         2) should also determine that everything equal to and less than the strength of the motors should be used
+         3) ... but should be put into another function*/
+    } 
+  }
+}
 
 void vibrate() {
   // TODO

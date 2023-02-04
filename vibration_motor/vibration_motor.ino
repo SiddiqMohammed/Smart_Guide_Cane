538dd95194415e8eaeef88292c4592cfa735207e
J-an777 <114760778+J-an777@users.noreply.github.com>
2023-02-04 15:53:56 -0500

    Update vibration_motor.ino

--- vibration_motor/vibration_motor.ino
+++ vibration_motor/vibration_motor.ino
@@ -3,8 +3,19 @@
 */
 
 int motorPin = 3; //motor transistor is connected to pin 3
-int n_pulse = 3;
-int pulse_strength = 0;
+
+// Pulse Vars (May need to delete some for memory purposes)
+int pulse_Min = 80;
+int pulse_Max = 255;
+int pulse_Num = 0;
+int min_pulses = 1;
+int max_pulses = 4;
+int q_increment = (pulse_Max - pulse_Min) / 4;  // quadrant incrementor
+int q1_max = (int) (pulse_Min + q_increment);   // quadrant bounds
+int q2_max = (int) (q1_max + q_increment);
+int q3_max = (int) (q2_max + q_increment);
+
+
 
 void setup()
 {
@@ -15,19 +26,25 @@ void setup()
 
 void loop()
 {
-  // Random Number Generator
-  sonar_dist = rand(0, 300);
-  
-  // Get pulse strength
-  pulse_strength = map(sonar_dist, 0, 300, 80, 255);
-    
-  // Depending on values, give a certain number of pulses and forward the pulse_strength
-  if (pulse_strength > 250);
-  {
-    vibrate_old(4, pulse_strength);
-  }
+  // Generate a sonar distance and map it to pulse_strength.
+  int sonar_Dist = rand(0, 300);
+  int pulse_Strength = map(sonar_Dist, 0, 300, pulseMin, pulseMax);
+ 
+
+  // Map pulse_Strength to pulse_Num with proper ranges.
+  if(pulse_Strength > q3_max)
+    pulse_Num = map(pulse_Strength, q3_max, pulse_Max, min_pulses, max_pulses);
+  else if(pulse_Strength > q2_max)
+    pulse_Num = map(pulse_Strength, q2_max, q3_max, min_pulses, max_pulses);
+  else if(pulse_Strength > q1_max)
+    pulse_Num = map(pulse_Strength, q1_max, q2_max, min_pulses, max_pulses);
   else
-    vibrate_old(3, pulse_strength);
+    pulse_Num = map(pulse_Strength, pulse_Min, q1_max, min_pulses, max_pulses);
+
+  // Vibrate with achieved values.
+  vibrate_old(pulse_Num, pulse_Strength);
+  
+  
   
   // TODO: Create a similar function as vibrate_old() but make it without delay
   // Currently the vibrate_old function uses delays and this blocks the code
@@ -37,14 +54,14 @@ void loop()
   // vibrate();
 
 
-  vibrate_old(3, 80);
+  /*vibrate_old(3, 80);
   delay(1000);
   vibrate_old(3, 120);
   delay(1000);
   vibrate_old(3, 200);
   delay(1000);
   vibrate_old(4, 250);
-  delay(1000);
+  delay(1000);*/
 
 }
 
@@ -67,7 +84,6 @@ void vibrate_old(int n_pulse, int pulse_strength) {
 }
 
 
-void vibrate() 
-{
-  
+void vibrate() {
+  // TODO
 }
